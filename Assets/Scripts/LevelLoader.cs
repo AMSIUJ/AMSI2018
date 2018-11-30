8e1ae8e3ad7e6b552dd5d89e69bb332962a065bf
Benek <ludomirc@beniamin.czaplicki@gmail.com>
2018-11-30 06:51:17 +0100

    added level loader

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public string sceneToLoad = "StartScreen";
+
+
+    public void loadTheLevel()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
